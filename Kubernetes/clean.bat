9dd526384f0825fb4ff3ec7ee2841ec514b84f1b
arjunamar5 <arjunamar5@gmail.com>
2025-10-20 19:34:08 +0530

    Added k8s fixes and added extra commands in .bat file

--- Kubernetes/clean.bat
+++ Kubernetes/clean.bat
@@ -1,12 +1,9 @@
 kubectl delete -f mongo-pv.yaml
-
+kubectl delete -f rbac.yaml
 kubectl delete -f mongo-deployment.yaml
 kubectl delete -f mongo-service.yaml
 kubectl delete -f app-deployment.yaml
 kubectl delete -f app-service.yaml
 kubectl delete -f mongo-pvc.yaml
-
-
-REM To check for objects in k8s
 
 kubectl get all
--- Kubernetes/deploy.bat
+++ Kubernetes/deploy.bat
@@ -2,5 +2,8 @@ kubectl apply -f mongo-pv.yaml
 kubectl apply -f mongo-pvc.yaml
 kubectl apply -f mongo-deployment.yaml
 kubectl apply -f mongo-service.yaml
+kubectl apply -f rbac.yaml
 kubectl apply -f app-deployment.yaml
 kubectl apply -f app-service.yaml
+
+kubectl get all
